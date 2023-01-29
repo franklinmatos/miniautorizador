edd4c585de9fd634fd8728665bd9dca71fdf5e17
R80D\franklinmatos <franklinmatos@brq.com>
2023-01-29 00:03:17 -0300

    criando classes base e classes de teste

---
+++ src/main/java/com/microautorizador/MiniAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package com.microautorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microautorizador/test/service/CartaoServiceTest.java
@@ -0,0 +1,36 @@
+package com.microautorizador.test.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CartaoServiceTest {
+	
+	@Test
+	void pesquisarNumeroCartao_sucesso() {
+
+	}
+
+	@Test
+	void pesquisarNumeroCartao_erro() {
+
+	}
+
+	@Test
+	void criarCartaoNovo_sucesso(){
+
+	}
+
+	@Test
+	void criarCartaoNovo_erro(){
+
+	}
+
+	@Test
+	void salvarCartaoNovo_sucesso(){
+
+	}
+
+	@Test
+	void salvarCartaoNovo_erro(){
+
+	}
+}
